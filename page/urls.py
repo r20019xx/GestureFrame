30bbcc77cea515b54b15e765b653aacdf19adc10
vli1 <vli1@vt.edu>
2025-03-28 15:07:13 -0400

    added a login/logout form with user authentication using sessions to store the username and role

--- page/urls.py
+++ page/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     # recipe views
     path('', views.homeview, name='homeview'),
     path('about/', views.aboutview, name='aboutview'),
+    path('logout', views.logout, name='logout'),
+    path('login/', views.login, name='login'),
 ]
