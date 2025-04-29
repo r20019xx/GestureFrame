e5910141441e67710b10f4789d37730b42591fd8
vli1 <vli1@vt.edu>
2025-04-29 14:54:01 -0400

    Created tests for new registered users

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Detail
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    # Newly registed user test
+    def setUp(self):
+        User.objects.create(username="victor", email="<EMAIL>", password="<PASSWORD>")
+
+    def test_user_in_session(self):
+        user = User.objects.get(username="victor")
+        self.assertEqual(user.username, "victor")
+        self.assertEqual(user.email, "<EMAIL>")
+        self.assertEqual(user.password, "<PASSWORD>")
+        self.assertEqual(user.detail.role, "regular")
